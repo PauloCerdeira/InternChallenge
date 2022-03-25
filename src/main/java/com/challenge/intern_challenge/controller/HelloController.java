7d3604a35b9acd2e5b01567316795e18285b0764
Paulo Cerdeira <paulocerdeira1999@gmail.com>
2022-03-25 14:41:33 -0300

    criação de arquivo e envio do JSON implementados

--- src/main/java/com/challenge/intern_challenge/controller/HelloController.java
+++ src/main/java/com/challenge/intern_challenge/controller/HelloController.java
@@ -1,6 +1,12 @@
 package com.challenge.intern_challenge.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import net.minidev.json.JSONObject;
+
+import java.io.File;
+import java.io.PrintWriter;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,18 +18,48 @@ public class HelloController {
     }
 
     @GetMapping(value = "/createNewFile")
-    public String createNewFile(@RequestParam(value = "name", defaultValue = "Mundo") String name) {
-        return "criar new file!";
+    public String createNewFile() {
+        try {
+            File file = new File("C:\\temp\\file.csv");
+            PrintWriter pw = new PrintWriter(file);
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("sl_no");
+            sb.append(";");
+            sb.append(" book name");
+            sb.append(";");
+            sb.append(" category");
+            sb.append(";");
+            sb.append("\n");
+            sb.append("sl_no");
+            sb.append(";");
+            sb.append(" book name");
+            sb.append(";");
+            sb.append(" category");
+            sb.append(";");
+
+            pw.write((sb.toString()));
+            pw.close();
+
+            System.out.println("File created at " + file.getPath());
+            return file.getPath();
+        } catch (Exception error) {
+            System.out.println(error);
+            return "Unexpected Error. Try again later.";
+        }
+
     }
 
     @GetMapping(value = "/financialByTrader")
-    public String getFinancialByTrader() {
-        return "retornar financial by trader!";
-    }
+    public JSONObject financialByTrader() {
+        JSONObject obj = new JSONObject();
 
-    @GetMapping(value = "/error")
-    public String getError() {
-        return "Unexpected error. Try again Later!";
+        obj.put("name", "foo");
+        obj.put("num", 100);
+        obj.put("balance", 1000.21);
+        obj.put("is_vip", true);
+
+        return obj;
     }
 
 }
